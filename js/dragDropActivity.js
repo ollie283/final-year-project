3373c1d6f555f23349d35399a950360a62428331
Oliver Holland <odholl@essex.ac.uk>
2017-02-14 13:44:31 +0000

    added function hide the error and success feedback box. also to increase the size of the drop area each time a object/label is added

--- js/dragDropActivity.js
+++ js/dragDropActivity.js
@@ -1,4 +1,23 @@
+/*
+ * Drag & Drop Activity
+ *
+ */
+
 $(function () {
+
+    $('#feedback_error').hide();
+    $('#feedback_success').hide();
+
+    var holder = 0;
+    $(".drop_target ul").each(function () {
+        if ($(this).height() > holder) {
+            holder = ($(this).height() + 10);
+        }
+    });
+
+    $(".drop_target ul").each(function () {
+        $(this).height(holder);
+    });
 
     // START randomise the order of the activities
     $.fn.randomize = function (selector) {
@@ -16,6 +35,6 @@ $(function () {
     // END randomise the order of the activities
 
     $('#act_drag_items').randomize();
-    $(".draggable").draggable({revert: true, cursor: "move", opacity: 0.8});
-    $(".drop_target").droppable({drop: handle_label_drop, hoverClass: "drop-hover"});
+    $('.draggable').draggable({revert: true, cursor: "move", opacity: 0.8});
+    $('.drop_target').droppable({drop: handle_label_drop, hoverClass: "drop-hover"});
 });
