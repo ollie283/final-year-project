25eb36ab73fe69740e7bc3beb6810e24dd40142e
Oliver Holland <odholl@essex.ac.uk>
2017-02-14 14:00:19 +0000

    added a new function that will handle the label drop and declared the appropraite variables

--- js/dragDropActivity.js
+++ js/dragDropActivity.js
@@ -8,14 +8,15 @@ $(function () {
     $('#feedback_error').hide();
     $('#feedback_success').hide();
 
+    // Increase size of drop area each time a object is added
     var holder = 0;
-    $(".drop_target ul").each(function () {
+    $(".drop_target ul").each(function (index) {
         if ($(this).height() > holder) {
             holder = ($(this).height() + 10);
         }
     });
 
-    $(".drop_target ul").each(function () {
+    $(".drop_target ul").each(function (index) {
         $(this).height(holder);
     });
 
@@ -35,6 +36,20 @@ $(function () {
     // END randomise the order of the activities
 
     $('#act_drag_items').randomize();
+
+    //make objects draggable
     $('.draggable').draggable({revert: true, cursor: "move", opacity: 0.8});
+
+    // make drop area droppable
     $('.drop_target').droppable({drop: handle_label_drop, hoverClass: "drop-hover"});
+
+    //get original positions of drag elements
+    function handle_label_drop(event, ui) {
+        var drop_target_value = $(this).data('type');
+        var label_value = ui.draggable.data('activity');
+        $('#feedback_error').hide();
+        $('#feedback_success').hide();
+        
+    }
 });
+
