8f3fda4a1de24514f8056ecbb98fb19ec28c674f
Oliver Holland <odholl@essex.ac.uk>
2017-02-20 18:38:04 +0000

    display supoorting code tool bar after header

--- js/textTools.js
+++ js/textTools.js
@@ -4,7 +4,9 @@ $().ready(function () {
     //Display toolbar
     var toolbar_buttons = '<div id="tools" class="pull-right">' +
         '<div id="supporting_text_tools" class="supporting_code_control"><span class="glyphicon glyphicon-align-justify"></span>' +
-        '<span class="tool_text">Supporting Code</span><a href="#" class="btn btn-default btn-xs" data-toggle="tooltip" data-placement="top" title="Hide Supporting Code" id="show-hide-code">' +
+        '<span class="tool_text"> Supporting Code </span><a href="#" class="btn btn-default btn-xs" data-toggle="tooltip" data-placement="top" title="Hide Supporting Code" id="show-hide-code">' +
         '<span class="glyphicon glyphicon-eye-close"</span></a></div>';
+
+    $('header:first').after(toolbar_buttons);
 
 });
