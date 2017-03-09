da3b4727245a143c7fbd446a3fc7dbf540339d22
Oliver Holland <odholl@essex.ac.uk>
2017-03-09 10:51:22 +0000

    created a function that will act as the canvas and all other functions will inherit from this making use of 'prototype'. Also added a call for the main program

---
+++ js/algorithmsInAction.js
@@ -0,0 +1,25 @@
+/*!
+ * Algorithms In Action
+ *
+ * This file contains the functionality that
+ * controls the visualization of the sorting
+ * algorithms on the algorithmsInAction.html page
+ *
+ */
+
+'use strict';
+
+/*******************************
+ * Algorithms In Action Canvas *
+ *******************************/
+function AlgorithmsInAction(canvasID, size) {
+
+}
+
+
+/****************
+ * Main Program *
+ ****************/
+(function() {
+    new AlgorithmsInAction('algorithms_in_action', 40);
+})();
